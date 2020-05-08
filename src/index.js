f6f6ef8512e2cb351f412fba71a5759caf0356c7
Your Name <you@example.com>
2020-05-08 08:39:02 +0000

    Fix small bug

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { StylesProvider } from '@material-ui/styles';
 // import firebase from 'firebase';
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCnIUVm1Yw0DGmza0BYRqzhtU_x1ZIe1bA",
-//   authDomain: "todolist-bc9ed.firebaseapp.com",
-//   databaseURL: "https://todolist-bc9ed.firebaseio.com",
-//   projectId: "todolist-bc9ed",
-//   storageBucket: "",
-//   messagingSenderId: "720192206141",
-//   appId: "1:720192206141:web:e2fed1410f3d38d9"
+//   apiKey: XXXXXXXXXXXXXXXx,
+//   authDomain: XXXXXXXXXXXXXXXx,
+//   databaseURL: XXXXXXXXXXXXXXXx,
+//   projectId: XXXXXXXXXXXXXXXx,
+//   storageBucket: XXXXXXXXXXXXXXXx,,
+//   messagingSenderId: XXXXXXXXXXXXXXXx,
+//   appId: XXXXXXXXXXXXXXXx
 // };
 
 // firebase.initializeApp(firebaseConfig);
